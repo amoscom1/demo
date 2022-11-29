8343577327768c650fb9882a19564dde0e45afb5
amoscom <edsanbus@gmail.com>
2022-11-29 18:54:28 +0000

    Added the pharse "Practice makes perfect"

--- demo1.java
+++ demo1.java
@@ -1,7 +1,8 @@
 class Demo{
        public static void main(String[] args) {
 	      System.out.println("This is a demo program.");
-	     System.out.println("Git is very easy to operate");
-	    System.out.println("I am still learining this git thing"); 
+	      System.out.println("Git is very easy to operate");
+	      System.out.println("I am still learining this git thing"); 
+	      System.out.println("Practice makes perfect");
        }	
 }
