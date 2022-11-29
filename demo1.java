a1fd423292322ae43fb6b2f04194ab4e05a3b69b
amoscom <edsanbus@gmail.com>
2022-11-29 06:58:38 +0000

    Modified demo1 file which was added previously.

--- demo1.java
+++ demo1.java
@@ -1,6 +1,7 @@
 class Demo{
        public static void main(String[] args) {
 	      System.out.println("This is a demo program.");
-	     System.out.println("Git is very easy to operate"); 
+	     System.out.println("Git is very easy to operate");
+	    System.out.println("I am still learining this git thing"); 
        }	
 }
